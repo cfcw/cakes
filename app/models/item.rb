0fdfd465ffaadecdfe5ce3465bd4eb9f0402fa1c
H.Tajima <tajihiro2@gmail.com>
2014-03-22 00:04:21 +0900

    implimente reputation.

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,6 @@
 class Item < ActiveRecord::Base
+  belongs_to :shop
+  has_many :reputations
+  
   attr_accessible :item_name
 end
--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,3 +1,4 @@
 class Shop < ActiveRecord::Base
-  attr_accessible :create_user_id, :shop_name
+  attr_accessible :create_user_id, :shop_name, :zip_cd, :address, :phone, :start_time, :end_time, :time_info,
+                  :shop_info, :memo1, :memo2
 end
